13c2dd3e177190e3e19b39252b0d44c22cf3caca
Alexander <ksandr.lr@gmail.com>
2021-05-06 15:06:48 +0300

    add color change

--- Assets/Scripts/PointCounter.cs
+++ Assets/Scripts/PointCounter.cs
@@ -11,9 +11,15 @@ public class PointCounter : MonoBehaviour
     public State state;
     public float groundLevelY = 0.16f;
     public enum State {OnGround, InAir}
+    public Renderer cubeMaterial;
+    public Material onGroundCubeMaterial;
+    public Material inAirCubeMaterial;
     // Start is called before the first frame update
     void Start()
     {
+        //onGroundCubeMaterial = Resources.Load("Assets/box_material.mat", typeof(Material)) as Material;
+        //inAirCubeMaterial = Resources.Load("Assets/ground_material.mat", typeof(Material)) as Material;
+        cubeMaterial = cube.GetComponent<Renderer>();
         state = isOnGround() ? State.OnGround : State.InAir;
         points = 0;
     }
@@ -31,6 +37,9 @@ public class PointCounter : MonoBehaviour
             points--;
             state = State.InAir;
         }
+
+        cubeMaterial.material = state == State.InAir ? inAirCubeMaterial : onGroundCubeMaterial;
+            
 
     }
 
